3d9efc1b5de34f4125ce66cfe34c8192509ab984
VSXR <vsnzdev@gmail.com>
2025-05-13 13:58:45 +0200

    fixed submision guides html

--- tinySteps/management/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-TinySteps Management System - Tools for managing the TinySteps application
-
-This package contains commands and utilities for content generation,
-notifications, translations, and other administrative tasks
-"""
-
-__version__ = "1.0.0"
-__author__ = "TinySteps Team"
